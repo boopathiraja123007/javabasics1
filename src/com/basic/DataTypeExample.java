2086afa77909ac003396f09f083746f285dbeeae
boopathiraja123007 <vijayaboopathi18@gmail.com>
2018-05-29 03:09:59 -0700

    string concat

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String s[])
+	{
+		String a="Hai guys";
+		String b=" This is eclipse";
+		System.out.println(a+b);
+	}
+}
