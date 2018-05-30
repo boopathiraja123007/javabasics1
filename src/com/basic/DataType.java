3977f21b7ced7c663a47455259b88a572d1650a0
boopathiraja123007 <vijayaboopathi18@gmail.com>
2018-05-29 21:16:28 -0700

    if else

---
+++ src/com/basic/DataType.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		{  int a=10;
+		if(a%2==0)
+		{
+			System.out.println(a+" is even" );
+		}
+		else
+		{
+			System.out.println(a+" is odd");
+		}
+		}
+	}
+		// TODO Auto-generated method stub
+
+	}
+
