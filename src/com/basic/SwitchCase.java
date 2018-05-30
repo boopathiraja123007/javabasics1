2d20fcef851be0d45236a60f333f01a8e3fd3b08
boopathiraja123007 <vijayaboopathi18@gmail.com>
2018-05-29 21:37:48 -0700

    SwitchCase

---
+++ src/com/basic/SwitchCase.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class SwitchCase {
+	public static void main(String s[])
+	{
+		int a=3;
+		switch(a)
+		{
+		case 1: System.out.println("one");break;
+		case 2:System.out.println("two");break;
+		case 3:System.out.println("three");break;
+	    default:System.out.println("default");break;
+		}
+	}
+
+}
