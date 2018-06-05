9e2b04d8b0d40732c9fe0a5b70ae28f89c2677cf
boopathiraja123007 <vijayaboopathi18@gmail.com>
2018-06-05 12:55:03 -0700

    fin min and max values of array

--- src/com/basic/Abc.java
+++ src/com/basic/Abc.java
@@ -1,25 +1,48 @@
 package com.basic;
 
 public class Abc {
-
-	
-
-	public static void main(String[] args) {
-		int[] a= {3,4,5,2,33,24,2};
-		int min=a[0];
-		
-		for(int i=0;i<a.length;i++)
+ 
+	public void findMax(int string[])
+	{
+		int max=string[0];
+		for(int i=0;i<string.length;i++)
 		{
-			if(a[i]>min)
+			
+			if (string[i] >max)
 			{
-				min=a[i];        
+				max=string[i];        
 				
 			}
 			
 		}
+		System.out.println(max);
+	}
+	public void findMin(int string[])
+	{
+		int min=string[0];
+		for(int i=0;i<string.length;i++)
+		{
+			
+			if (string[i] < min)
+			{
+				min=string[i];        
+				
+			}
+			
+		}
+		System.out.println(min);
+	}
+	
+
+	public static void main(String[] args) {
+		int[] a= {3,4,5,2,33,24,2};
+	
+		Abc a1=new Abc();
+		a1.findMax(a);
+		a1.findMin(a);
 		
 			
-		System.out.println(min);
+		
 	
 	}
 }
