e55ae08fd3054d2042bb9d5d4accf8226c81549c
boopathiraja123007 <vijayaboopathi18@gmail.com>
2018-06-19 15:45:10 +0530

    ARRAYREVERSE

--- src/com/basic/ArrayReverse.java
+++ src/com/basic/ArrayReverse.java
@@ -1,23 +1,34 @@
 package com.basic;
 
 public class ArrayReverse {
-     
-	public void reverse(int a[])
-	{int b[]= {};
-		for(int i=a.length;i<0;i--,a.length--)
-		{
-			
-			b[a.length]=a[i];
-		}
-		for(int i=0;i<b.length;i++)
-		{
-			System.out.println(b[i]);
-		}
-	}
+//     
+//	public void reverse(int a[])
+//	{int b[]= {};
+//		for(int i=a.length;i<0;i--)
+//		{
+//			
+//			b[a.length]=a[i];
+//			
+//		}
+//		for(int i=0;i<b.length;i++)
+//		{
+//			System.out.println(b[i]);
+//		}
+//	}
+//	public static void main(String[] args) {
+//		int a[]= {3,4,5,2,33,24,2};
+//		ArrayReverse c=new ArrayReverse();
+//		c.reverse(a);
+//	}
+//	
 	public static void main(String[] args) {
-		int a[]= {3,4,5,2,33,24,2};
-		ArrayReverse c=new ArrayReverse();
-		c.reverse(a);
+		int arr[]= {1,2,4,2,56,78,23,68};
+		int arr1[]=new int[arr.length];
+		System.arraycopy(arr,2 ,arr1,0,arr.length-2);
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.println(arr1[i]);
+			
+		}
 	}
 	
 }
